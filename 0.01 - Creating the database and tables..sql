3bd2ff03190641ab68a17f211c6df98ca246371a
Varsha Ganesan <ar.varshaganesan@gmail.com>
2024-01-04 18:04:06 +0530

    Add files via upload

---
+++ 0.01 - Creating the database and tables..sql
@@ -0,0 +1,72 @@
+-- Project: Simplified Instragram_clone
+-- Creating the database and respective tables
+CREATE DATABASE instagram_clone;
+USE instagram_clone;
+SELECT DATABASE();
+
+-- Creating the User table
+CREATE TABLE users(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	username VARCHAR (255) UNIQUE NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW()
+);
+DESC users;
+
+-- Creating the Photos table
+CREATE TABLE photos(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	image_url VARCHAR(255) NOT NULL,
+	user_id INT NOT NULL,
+	created_at TIMESTAMP DEFAULT NOW(),
+	FOREIGN KEY(user_id) REFERENCES users(id)
+);
+
+-- Creating the comments table
+CREATE TABLE comments(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	comment_text VARCHAR(255) NOT NULL,
+	user_id INT NOT NULL,
+	photo_id INT NOT NULL,
+	created_at TIMESTAMP DEFAULT NOW(),
+	FOREIGN KEY(user_id) REFERENCES users(id),
+	FOREIGN KEY(photo_id) REFERENCES photos(id)
+);
+
+-- Creating the likes table. Adding a primary key constraint to restrict more than one likes by the same user.
+CREATE TABLE likes(
+	user_id INT NOT NULL,
+	photo_id INT NOT NULL,
+	created_at TIMESTAMP DEFAULT NOW(),
+	FOREIGN KEY(user_id) REFERENCES users(id),
+	FOREIGN KEY(photo_id) REFERENCES photos(id),
+    PRIMARY KEY(user_id , photo_id)
+);
+
+-- Creating followers and following table
+CREATE TABLE follows(
+	follower_id INT NOT NULL,
+    followee_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY(follower_id) REFERENCES users(id),
+    FOREIGN KEY(followee_id) REFERENCES users(id)
+);
+
+-- Creating hashtags table
+CREATE TABLE tags(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    tag_name VARCHAR(255) UNIQUE,
+    created_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE photo_tags(
+	photo_id INT NOT NULL,
+    tag_id INT NOT NULL,
+    FOREIGN KEY(photo_id) REFERENCES photos(id),
+    FOREIGN KEY(tag_id) REFERENCES tags(id),
+    PRIMARY KEY(photo_id , tag_id)
+);
+
+
+    
+
+
